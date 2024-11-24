e7499bccbeedd041e0a97f6c2d3b9961e7a6878a
sunakan <sunakan03@gmail.com>
2024-11-24 09:01:08 +0900

    rails: Transaction内で、DB再接続の挙動チェック用メソッドを生やす
    参考: https://shmokmt.hatenablog.com/entry/2021/10/25/171424

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  def self.create_with_transaction_reconnect
+    debugger
+    ActiveRecord::Base.transaction do
+      foo = User.new(name: 'foo')
+      foo.save!
+      ActiveRecord::Base.connection.reconnect!
+      bar = User.new(name: 'bar')
+      bar.save!
+    end
+  end
+
+  def self.create_with_transaction
+    ActiveRecord::Base.transaction do
+      foo = User.new(name: 'foo')
+      foo.save!
+      bar = User.new(name: 'bar')
+      bar.save!
+    end
+  end
 end
